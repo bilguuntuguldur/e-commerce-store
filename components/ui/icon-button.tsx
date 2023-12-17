e02f3e665267e5916b7afacc35f1cefede6053f6
bilguuntuguldur <bilguutuguldur.0217@gmail.com>
2023-12-18 00:30:36 +0800

    Icon button component fix type issue

--- components/ui/icon-button.tsx
+++ components/ui/icon-button.tsx
@@ -1,28 +1,23 @@
 import { cn } from "@/lib/utils";
-import { MouseEventHandler } from "react";
 
 interface IconButtonProps {
-    onClick?: MouseEventHandler<HTMLAnchorElement> | undefined;
-    icon: React.ReactElement;
-    className?: string
+  onClick?: React.MouseEventHandler<HTMLElement> | undefined;
+  icon: React.ReactElement;
+  className?: string;
 }
 
-const IconButton: React.FC<IconButtonProps> = ({
-onClick,
-icon,
-className
-}) => {
-    return (
-        <button
-            onClick={onClick}
-            className={cn(
-                "rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition",
-                className
-            )}
-        >
-            {icon}
-        </button>
-    )
-}
+const IconButton: React.FC<IconButtonProps> = ({ onClick, icon, className }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition",
+        className
+      )}
+    >
+      {icon}
+    </button>
+  );
+};
 
 export default IconButton;
